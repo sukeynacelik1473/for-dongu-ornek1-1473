09f4523f504ce377b76e2c3da3292b82e7a1f95d
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-23 12:20:06 +0300

    Add files via upload

---
+++ fordongüsü-örnek1-1473/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace fordongüsü_örnek1_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lbAd = new System.Windows.Forms.ListBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEkle.Location = new System.Drawing.Point(25, 97);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(113, 53);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "YAZ";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // lbAd
+            // 
+            this.lbAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbAd.FormattingEnabled = true;
+            this.lbAd.ItemHeight = 16;
+            this.lbAd.Location = new System.Drawing.Point(154, 33);
+            this.lbAd.Name = "lbAd";
+            this.lbAd.Size = new System.Drawing.Size(120, 244);
+            this.lbAd.TabIndex = 1;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(25, 52);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(113, 22);
+            this.txtAd.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 319);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lbAd);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lbAd;
+        private System.Windows.Forms.TextBox txtAd;
+    }
+}
+
---
+++ fordongüsü-örnek1-1473/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fordongüsü_örnek1_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+
+            for(int i = 0; i < 10; i++)
+            {
+                string metin = i.ToString() + "😍 🥰" + ad;
+                lbAd.Items.Add(metin);
+            }
+        }
+    }
+}
